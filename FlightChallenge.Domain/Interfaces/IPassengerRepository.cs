63ba87c93995800c4ba06fc08d1006c0c4c54c98
0859917241 <0859917241@KR011-9990C2001>
2025-01-08 16:32:28 +0330

    complete passenger repository

--- FlightChallenge.Domain/Interfaces/IPassengerRepository.cs
+++ FlightChallenge.Domain/Interfaces/IPassengerRepository.cs
@@ -1,7 +1,14 @@
-﻿namespace FlightChallenge.Domain.Interfaces
+﻿using FlightChallenge.Domain.Entities;
+
+namespace FlightChallenge.Domain.Interfaces
 {
     public interface IPassengerRepository
     {
+        Task<Passenger> GetPassengerByIdAsync(int id); 
+        Task<IEnumerable<Passenger>> GetPassengersAsync(); 
+        Task AddPassengerAsync(Passenger passenger);  
+        Task UpdatePassengerAsync(Passenger passenger); 
+        Task DeletePassengerAsync(int id); 
     }
 
 }
--- FlightChallenge.Infrastructure/Repositories/PassengerRepository.cs
+++ FlightChallenge.Infrastructure/Repositories/PassengerRepository.cs
@@ -1,9 +1,33 @@
-﻿using FlightChallenge.Domain.Interfaces;
+﻿using FlightChallenge.Domain.Entities;
+using FlightChallenge.Domain.Interfaces;
 
 namespace FlightChallenge.Infrastructure.Repositories
 {
     public class PassengerRepository : IPassengerRepository
     {
+        public Task AddPassengerAsync(Passenger passenger)
+        {
+            throw new NotImplementedException();
+        }
 
+        public Task DeletePassengerAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Passenger> GetPassengerByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Passenger>> GetPassengersAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdatePassengerAsync(Passenger passenger)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
